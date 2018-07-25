b6c2ccc063c4ee5cc8054296906af5d06a746b27
RenChern <lincolnchern@gmail.com>
2018-07-25 17:31:03 -0300

    Add files via upload

---
+++ zcr.sh
@@ -0,0 +1,11 @@
+sudo apt update -y
+sudo apt upgrade -y
+wget https://zcore.ch/downloads/Linux-x64/ZCore_linux64.tar.gz
+tar -xf ZCore_linux64.tar.gz
+chmod 777 ./dependencies.sh
+./dependencies.sh
+mkdir $HOME/.zcore
+YOURIP=`wget -qO- eth0.me`
+printf "rpcuser=zcoreuser\nrpcpassword=zcorepassword\nrpcport=51314\nrpcallowip=127.0.0.1\ndaemon=1\nlisten=1\nserver=1\nmaxconnections=256\nexternalip=$YOURIP\nmasternode=1\nmasternodeprivkey=" > /$HOME/.zcore/zcore.conf
+
+
